12358c4a9111992affd68cfecf5c1ab9e000e28d
GaoJiuJiu <929226915@qq.com>
2026-05-26 22:39:09 +0800

    fix: API地址改为相对路径，解决公网访问跨域问题

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 // API 配置
-// 部署时修改为服务器 IP 地址，如：http://192.168.1.100:3010
-export const API_BASE = 'http://localhost:3010'
+// 空字符串表示使用相对路径（前后端同域部署）
+export const API_BASE = ''
 
 // 是否使用云端模式（true = 云端同步，false = 本地 localStorage）
 export const USE_CLOUD = true
